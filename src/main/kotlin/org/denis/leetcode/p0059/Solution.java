78af479eda61363d826619be06f68ceb77317328
Denis Zhdanov <denzhdanov@gmail.com>
2019-07-20 15:56:31 +0800

    problem 59

--- src/main/kotlin/org/denis/leetcode/p0059/Solution.java
+++ src/main/kotlin/org/denis/leetcode/p0059/Solution.java
@@ -1,38 +1,35 @@
-package org.denis.leetcode.p0054;
-
-import java.util.ArrayList;
-import java.util.List;
+package org.denis.leetcode.p0059;
 
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().spiralOrder(new int[][] {
-//                {1, 2, 3},
-//                {4, 5, 6},
-//                {7, 8, 9}
-                {1,  2,  3, 4},
-                {5,  6,  7, 8},
-                {9, 10, 11, 12}
-        }));
+        int[][] result = new Solution().generateMatrix(4);
+        for (int[] ints : result) {
+            for (int i : ints) {
+                System.out.printf("%2d ", i);
+            }
+            System.out.println();
+        }
     }
 
-    private static final State TOP = new State(true, 1, 0, 0, 0, 0, 0);
-    private static final State RIGHT = new State(true, 0, 1, 0, 0, 0, 0);
+    private static final State TOP    = new State(true, 1, 0, 0, 0, 0, 0);
+    private static final State RIGHT  = new State(true, 0, 1, 0, 0, 0, 0);
     private static final State BOTTOM = new State(false, -1, 0, 0, 0, 1, -1);
-    private static final State LEFT = new State(false, 0, -1, 1, -1, 0, 0);
+    private static final State LEFT   = new State(false, 0, -1, 1, -1, 0, 0);
 
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
-        if (matrix.length == 0 || matrix[0].length == 0) {
+    public int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        if (n == 0) {
             return result;
         }
         State state = TOP;
         int lowRowLimit = -1;
-        int highRowLimit = matrix.length;
+        int highRowLimit = n;
         int row = 0;
         int lowColumnLimit = -1;
-        int highColumnLimit = matrix[0].length;
+        int highColumnLimit = n;
         int column = 0;
+        int counter = 0;
         while (row > lowRowLimit && row < highRowLimit && column > lowColumnLimit && column < highColumnLimit) {
             int currentRowLimit;
             int currentColumnLimit;
@@ -43,8 +40,9 @@ public class Solution {
                 currentRowLimit = lowRowLimit;
                 currentColumnLimit = lowColumnLimit;
             }
-            for (; row != currentRowLimit && column != currentColumnLimit; row += state.rowIncrement, column += state.columnIncrement) {
-                result.add(matrix[row][column]);
+            for (; row != currentRowLimit
+                   && column != currentColumnLimit; row += state.rowIncrement, column += state.columnIncrement) {
+                result[row][column] = ++counter;
             }
             row -= state.rowIncrement;
             column -= state.columnIncrement;
@@ -74,12 +72,12 @@ public class Solution {
 
     private static class State {
         public final boolean naturalOrder;
-        public final int columnIncrement;
-        public final int rowIncrement;
-        public final int startColumnChange;
-        public final int endColumnChange;
-        public final int startRowChange;
-        public final int endRowChange;
+        public final int     columnIncrement;
+        public final int     rowIncrement;
+        public final int     startColumnChange;
+        public final int     endColumnChange;
+        public final int     startRowChange;
+        public final int     endRowChange;
 
         public State(boolean naturalOrder,
                      int columnIncrement,
