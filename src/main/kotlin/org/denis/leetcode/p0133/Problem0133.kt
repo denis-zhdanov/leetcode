7371bd402642b8ed0dbf0951b08ee98aab875a66
denis.zhdanov <denzhdanov@gmail.com>
2025-12-23 21:57:26 +0800

    problem 133

---
+++ src/main/kotlin/org/denis/leetcode/p0133/Problem0133.kt
@@ -0,0 +1,50 @@
+package org.denis.leetcode.p0133
+
+import java.util.Stack
+
+fun main() {
+
+}
+
+class Solution {
+    fun cloneGraph(node: Node?): Node? {
+        node ?: return node
+        val inputNodes = mutableMapOf<Int, Node>()
+        val resultNodes = mutableMapOf<Int, Node>()
+        val filled = mutableSetOf<Int>()
+        val inputToProcess = Stack<Node>()
+        inputToProcess.push(node)
+        while (inputToProcess.isNotEmpty()) {
+            val inputNode = inputToProcess.pop()
+            inputNodes[inputNode.`val`] = inputNode
+            val resultNode = resultNodes.computeIfAbsent(inputNode.`val`) {
+                Node(it)
+            }
+
+            if (!filled.add(inputNode.`val`)) {
+                continue
+            }
+
+            inputNode.neighbors.forEach { inputNeighbour ->
+                if (inputNeighbour != null) {
+                    inputToProcess.push(inputNeighbour)
+                    resultNode.neighbors += resultNodes.computeIfAbsent(inputNeighbour.`val`) {
+                        Node(it)
+                    }
+                }
+
+            }
+        }
+        return resultNodes[node.`val`]
+    }
+}
+
+data class Node(var `val`: Int) {
+    var neighbors: ArrayList<Node?> = ArrayList<Node?>()
+
+//    companion object {
+//        fun parse(s: String): Node {
+//
+//        }
+    }
+}
